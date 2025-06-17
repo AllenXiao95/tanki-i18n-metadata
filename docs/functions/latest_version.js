921921ce55f8e62b5b34c446a5e05aa20a91e882
Allen.Xiao <gas_steven@sina.com>
2025-06-17 14:02:21 +0800

    Delete docs/functions directory

--- docs/functions/latest_version.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const version = "2.1"; // 你也可以从 context.env 或环境变量中读取
-  return new Response(JSON.stringify({ version }), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-}
