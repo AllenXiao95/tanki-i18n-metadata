701536a428e1a97593443fc12c930b4318e55ce8
Allen.Xiao <geekxl@foxmail.com>
2025-09-25 15:48:47 +0800

    Update version numbers and last modified dates.
    - Update last modified date for homepage in sitemap.xml to '2025-09-25'
    - Update version number for "测试服" in version.config.js to 'v1.38.0.0' with date '2025-09-25'

--- docs/static/version.config.js
+++ docs/static/version.config.js
@@ -1,5 +1,5 @@
 // static/version.config.js
 window.VERSION_CONFIG = {
   release: { label: "正式服", version: "v1.37.0.1", date: "2025-09-13" },
-  test:    { label: "测试服", version: "v1.37.0.0", date: "2025-08-14" }
+  test:    { label: "测试服", version: "v1.38.0.0", date: "2025-09-25" }
 };
