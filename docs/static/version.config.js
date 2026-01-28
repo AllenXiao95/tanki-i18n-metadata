906c3f124064906460e0e3dd10deb4f68023a492
Allen.Xiao <geekxl@foxmail.com>
2026-01-28 17:18:49 +0800

    docs: 更新正式服版本号至v1.40.0.2

--- docs/static/version.config.js
+++ docs/static/version.config.js
@@ -1,5 +1,5 @@
 // static/version.config.js
 window.VERSION_CONFIG = {
-  release: { label: "正式服", version: "v1.40.0.0", date: "2026-01-28" },
+  release: { label: "正式服", version: "v1.40.0.2", date: "2026-01-28" },
   test:    { label: "测试服", version: "v1.40.0.4", date: "2026-01-15" }
 };
